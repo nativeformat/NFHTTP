e0a34fa7502bd0a68a3ac5315d4adab3e40fc0a9
sackfield <sackfield@spotify.com>
2018-08-13 14:20:10 -0400

    Remove linuxschroot shell script

--- ci/linuxschroot.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Create and hook for schroot destroy.
-SCHROOT_SESSION=$(schroot -b -c stable-amd64-trusty)
-trap "schroot -c $SCHROOT_SESSION -e" EXIT
-
-# Execute a script in schroot
-schroot -u "spotify-buildagent" -r -c $SCHROOT_SESSION -- sh ci/linux.sh
