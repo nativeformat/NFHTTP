3e1aa0ea29c1b13f375573b10f90f54acc5c2bcf
sackfield <sackfield@spotify.com>
2018-08-13 14:22:14 -0400

    Install clang-3.9 et al

--- ci/linux.sh
+++ ci/linux.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-# Must be run inside a schroot environment
-
 # Exit on any non-zero status
 set -e
 
 # Install system dependencies
-sudo apt-get install -y --no-install-recommends apt-utils
-sudo apt-get install -y clang-3.9
-sudo apt-get install -y clang-format-3.9
-sudo apt-get install -y libcurl4-openssl-dev
-sudo apt-get install -y libc++-dev
-sudo apt-get install -y ninja-build
+sudo apt-get update
+sudo apt-get install -y --no-install-recommends apt-utils \
+                                                clang-3.9 \
+                                                clang-format-3.9 \
+                                                libcurl4-openssl-dev \
+                                                libc++-dev \
+                                                ninja-build
 
 export CC=clang-3.9
 export CXX=clang++-3.9
