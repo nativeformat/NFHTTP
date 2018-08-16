e99a841db417d0f22ba50746b6e56da64c69eb00
sackfield <sackfield@spotify.com>
2018-08-16 13:59:08 -0400

    Add make_archive to nfbuild.py

--- ci/nfbuild.py
+++ ci/nfbuild.py
@@ -225,3 +225,13 @@ class NFBuild(object):
             if not multiple_files and len(matches) > 0:
                 break
         return matches
+
+    def make_archive(self, source, destination):
+        base = os.path.basename(destination)
+        name = base.split('.')[0]
+        format = base.split('.')[1]
+        archive_from = os.path.dirname(source)
+        archive_to = os.path.basename(source.strip(os.sep))
+        print(source, destination, archive_from, archive_to)
+        shutil.make_archive(name, format, archive_from, archive_to)
+        shutil.move('%s.%s'%(name,format), destination)
