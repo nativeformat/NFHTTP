22c51ee68a26bdffa7c46de4db32140c57e3dd49
sackfield <sackfield@spotify.com>
2018-08-28 13:23:45 -0400

    Add Windows Build

--- ci/windows.ps1
+++ ci/windows.ps1
@@ -1,3 +1,23 @@
+<#
+ * Copyright (c) 2018 Spotify AB.
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ #>
 param (
     [string]$build = "windows"
  )
@@ -5,38 +25,34 @@ param (
 Write-Host "NFHTTP build process starting..."
 Write-Host $build
 
-$ErrorActionPreference = "Stop"
-
 try
 {
-	# Get python version
-	$python_version = python --version
-	Write-Host $python_version
+	# Upgrade pip or else the CI will complain
+	c:\python27\python.exe -m pip install --upgrade pip
 
 	# Start virtualenv
-	$virtualenv_vulcan_output = python tools/vulcan/bin/vulcan.py -v -f tools/virtualenv.vulcan -p virtualenv-15.1.0
-	$virtualenv_bin = Join-Path $virtualenv_vulcan_output /virtualenv-15.1.0/virtualenv.py
-	python $virtualenv_bin nfdriver_env
+	pip install virtualenv
+	virtualenv nfhttp_env
 
-	& ./nfdriver_env/Scripts/activate.bat
+	& ./nfhttp_env/Scripts/activate.bat
 
 	# Install Python Packages
-	& nfdriver_env/Scripts/pip.exe install urllib3
-	& nfdriver_env/Scripts/pip.exe install pyyaml
-	& nfdriver_env/Scripts/pip.exe install flake8
-	& nfdriver_env/Scripts/pip.exe install cmakelint
+	& nfhttp_env/Scripts/pip.exe install urllib3 `
+										 pyyaml `
+										 flake8 `
+										 cmakelint
 
 	if($build -eq "android"){
-		& nfdriver_env/Scripts/python.exe ci/androidwindows.py
+		& nfhttp_env/Scripts/python.exe ci/androidwindows.py
 	} else {
-		& nfdriver_env/Scripts/python.exe ci/windows.py
+		& nfhttp_env/Scripts/python.exe ci/windows.py build
 	}
 
 	if($LASTEXITCODE -ne 0){
 		exit $LASTEXITCODE
 	}
 
-	& ./nfdriver_env/Scripts/deactivate.bat
+	& ./nfhttp_env/Scripts/deactivate.bat
 }
 catch
 {
--- ci/windows.py
+++ ci/windows.py
@@ -1,24 +1,76 @@
 #!/usr/bin/env python
+'''
+ * Copyright (c) 2018 Spotify AB.
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+'''
 
 import sys
 
 from nfbuildwindows import NFBuildWindows
+from build_options import BuildOptions
 
 
 def main():
+    buildOptions = BuildOptions()
+    buildOptions.addOption("installDependencies", "Install dependencies")
+    buildOptions.addOption("makeBuildDirectory",
+                           "Wipe existing build directory")
+    buildOptions.addOption("generateProject", "Regenerate project")
+    buildOptions.addOption("buildTargetCLI",
+                           "Build Target: CLI")
+    buildOptions.addOption("buildTargetLibrary", "Build Target: Library")
+    buildOptions.addOption("packageArtifacts", "Package the binary artifacts")
+    buildOptions.setDefaultWorkflow("Empty workflow", [])
+
+    buildOptions.addWorkflow("build", "Production Build", [
+        'installDependencies',
+        'makeBuildDirectory',
+        'generateProject',
+        'buildTargetCLI',
+        'buildTargetLibrary',
+        'packageArtifacts'
+    ])
+
+    options = buildOptions.parseArgs()
+    buildOptions.verbosePrintBuildOptions(options)
+
     library_target = 'NFHTTP'
     cli_target = 'NFHTTPCLI'
     nfbuild = NFBuildWindows()
-    nfbuild.build_print("Installing Dependencies")
-    nfbuild.installDependencies()
-    # Make our main build artifacts
-    nfbuild.build_print("C++ Build Start (x86)")
-    nfbuild.makeBuildDirectory()
-    nfbuild.generateProject()
-    targets = [library_target, cli_target]
-    for target in targets:
-        nfbuild.buildTarget(target)
-    # nfbuild.runIntegrationTests()
+
+    if buildOptions.checkOption(options, 'installDependencies'):
+        nfbuild.installDependencies()
+
+    if buildOptions.checkOption(options, 'makeBuildDirectory'):
+        nfbuild.makeBuildDirectory()
+
+    if buildOptions.checkOption(options, 'generateProject'):
+        nfbuild.generateProject(ios=True)
+
+    if buildOptions.checkOption(options, 'buildTargetLibrary'):
+        nfbuild.buildTarget(library_target)
+
+    if buildOptions.checkOption(options, 'buildTargetCLI'):
+        nfbuild.buildTarget(cli_target)
+
+    if buildOptions.checkOption(options, "packageArtifacts"):
+        nfbuild.packageArtifacts()
 
 
 if __name__ == "__main__":
