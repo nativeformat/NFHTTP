255f687e3300383337ccc544516abce40fd22f7f
Andrew Petersen <kirbysayshi@gmail.com>
2020-06-30 17:43:19 -0400

    try trusty again

--- ci/linux.sh
+++ ci/linux.sh
@@ -26,8 +26,8 @@ apt-get update
 apt-get install sudo
 sudo apt-get -q update
 sudo apt-get install -y -q --no-install-recommends apt-utils \
-    clang-3.9 \
-    clang-format-3.9 \
+    clang \
+    clang-format \
     libcurl4-openssl-dev \
     libc++-dev \
     ninja-build \
@@ -40,21 +40,9 @@ sudo apt-get install -y -q --no-install-recommends apt-utils \
     git \
     unzip \
     software-properties-common \
-    make
-
-# Extra repo for gcc-4.9 so we don't have to use 4.8
-sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
-sudo apt-get update
-sudo apt-get install -y --no-install-recommends gcc-4.9 \
-    g++-4.9 \
-    gobjc++-4.9
-
-sudo apt-get install -y --reinstall binutils
-
-# Install cmake 3.6.x
-wget --no-check-certificate https://cmake.org/files/v3.6/cmake-3.6.3-Linux-x86_64.sh -O cmake-3.6.3-Linux-x86_64.sh
-chmod +x cmake-3.6.3-Linux-x86_64.sh
-sudo sh cmake-3.6.3-Linux-x86_64.sh --prefix=/usr/local --exclude-subdir
+    make \
+    build-essentials \
+    cmake
 
 # Update submodules
 git submodule sync
