f97335b34ba48ea7a94e86d831fade5ddc591c13
Andrew Petersen <kirbysayshi@gmail.com>
2020-10-02 18:05:19 -0400

    fix mac ci

--- ci/osx.sh
+++ ci/osx.sh
@@ -23,8 +23,26 @@ set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
-# Install system dependencies
-brew install clang-format cmake ninja wget
+# Homebrew on circleci consistently fails with an error like:
+#
+# ==> Checking for dependents of upgraded formulae... Error: No such file or
+# directory - /usr/local/Cellar/git/2.26.2_1
+#
+# Completely unpredictable, because it's just homebrew cleaning itself up and
+# has nothing to do with the install itself! Just continue and hope the build
+# fails if one of these tools is not installed.
+if ! HOMEBREW_NO_AUTO_UPDATE=1 brew install \
+    clang-format@10 \
+    cmake \
+    ninja \
+    wget ; then
+    echo "Homebrew install had an error, review output and try manually."
+fi
+
+# Undo homebrew's potential meddling: https://github.com/pypa/pip/issues/5048
+# Homebrew will upgrade python to 3.8, but virtualenv hard codes the path to 3.7
+# in its shebang.
+pip3 uninstall --yes virtualenv && pip3 install virtualenv
 
 # Install virtualenv
 virtualenv --python=$(which python2) nfhttp_env
