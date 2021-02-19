e33206f0285a02e385df9497878e3258b141f252
Will Sackfield <will.sackfield@gmail.com>
2021-02-19 19:09:59 +0000

    Add strptime to windows

--- source/CachingSQLiteDatabase.cpp
+++ source/CachingSQLiteDatabase.cpp
@@ -26,6 +26,26 @@
 #include <sstream>
 #include <vector>
 
+#if defined(_WIN32) || defined(WIN32)
+extern "C" char* strptime(const char* s,
+                          const char* f,
+                          struct tm* tm) {
+  // Isn't the C++ standard lib nice? std::get_time is defined such that its
+  // format parameters are the exact same as strptime. Of course, we have to
+  // create a string stream first, and imbue it with the current C locale, and
+  // we also have to make sure we return the right things if it fails, or
+  // if it succeeds, but this is still far simpler an implementation than any
+  // of the versions in any of the C standard libraries.
+  std::istringstream input(s);
+  input.imbue(std::locale(setlocale(LC_ALL, nullptr)));
+  input >> std::get_time(tm, f);
+  if (input.fail()) {
+    return nullptr;
+  }
+  return (char*)(s + input.tellg());
+}
+#endif
+
 namespace nativeformat {
 namespace http {
 
